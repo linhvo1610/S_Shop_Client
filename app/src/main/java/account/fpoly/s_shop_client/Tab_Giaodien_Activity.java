ddc6a5cf0de579366d4a8a2f1d03cbf2cb335c06
vanhai0k <hainvph26725@fpt.edu.vn>
2023-09-26 20:25:35 +0700

    khung_tap

---
+++ app/src/main/java/account/fpoly/s_shop_client/Tab_Giaodien_Activity.java
@@ -0,0 +1,67 @@
+package account.fpoly.s_shop_client;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import account.fpoly.s_shop_client.fragment.GiohangFragment;
+import account.fpoly.s_shop_client.fragment.HomeFragment;
+import account.fpoly.s_shop_client.fragment.InfoUserFragment;
+import account.fpoly.s_shop_client.fragment.LichsuFragment;
+
+public class Tab_Giaodien_Activity extends AppCompatActivity {
+
+    BottomNavigationView navigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_giaodien);
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        Bundle bundle = new Bundle();
+        HomeFragment homeFragment = new HomeFragment();
+        homeFragment.setArguments(bundle);
+
+        InfoUserFragment infoUserFragment = new InfoUserFragment();
+        infoUserFragment.setArguments(bundle);
+
+        LichsuFragment lichsuFragment = new LichsuFragment();
+        lichsuFragment.setArguments(bundle);
+
+        GiohangFragment giohangFragment = new GiohangFragment();
+        giohangFragment.setArguments(bundle);
+
+
+        navigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.body_contaiber, new HomeFragment()).commit();
+        navigationView.setSelectedItemId(R.id.nav_home);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                if (item.getItemId() == R.id.nav_home) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.body_contaiber,homeFragment).commit();
+
+                }else if (item.getItemId() == R.id.nav_giohang) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.body_contaiber,giohangFragment).commit();
+
+                }else if (item.getItemId() == R.id.nav_tym) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.body_contaiber,lichsuFragment).commit();
+
+                }else if (item.getItemId() == R.id.nav_setting) {
+                    getSupportFragmentManager().beginTransaction().replace(R.id.body_contaiber,infoUserFragment).commit();
+
+                }
+                return true;
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/account/fpoly/s_shop_client/fragment/LichsuFragment.java
@@ -0,0 +1,26 @@
+package account.fpoly.s_shop_client.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import account.fpoly.s_shop_client.R;
+
+
+public class LichsuFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =inflater.inflate(R.layout.fragment_lichsu, container, false);
+
+
+        return view;
+    }
+}
