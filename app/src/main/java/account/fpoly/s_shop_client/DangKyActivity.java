719a991d5dcac895753c8a8a68eeab56e5a0bd2f
MinhQuan117 <minhquan1172003@gmail.com>
2023-10-02 21:59:43 +0700

    minhQuan_APILoginRegister

--- app/src/main/java/account/fpoly/s_shop_client/DangKyActivity.java
+++ app/src/main/java/account/fpoly/s_shop_client/DangKyActivity.java
@@ -4,12 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import account.fpoly.s_shop_client.Modal.UserModal;
+import account.fpoly.s_shop_client.Service.ServiceUser;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class DangKyActivity extends AppCompatActivity {
     Button btnDangKy;
+    private TextInputEditText dangky_username, dangky_phone, dangky_password;
+    private Retrofit retrofit;
+    private ServiceUser serviceUser;
+    private String url = "http://192.168.1.7:3000";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +43,37 @@ public class DangKyActivity extends AppCompatActivity {
                 overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
             }
         });
+    }
+
+    private void DangKy() {
+        retrofit = new Retrofit.Builder().baseUrl(url).addConverterFactory(GsonConverterFactory.create()).build();
+
+        String username = dangky_username.getText().toString().trim();
+        String phone = dangky_phone.getText().toString().trim();
+        String password = dangky_password.getText().toString().trim();
+
+        if (TextUtils.isEmpty(username)){
+            Toast.makeText(this, "Không được để trống Username", Toast.LENGTH_SHORT).show();
+        }else if (TextUtils.isEmpty(phone)){
+            Toast.makeText(this, "Không được để trống họ tên", Toast.LENGTH_SHORT).show();
+        }else if (TextUtils.isEmpty(password)){
+            Toast.makeText(this, "Không được để trống password", Toast.LENGTH_SHORT).show();
+        }else {
+            serviceUser = retrofit.create(ServiceUser.class);
+            Call<UserModal> call = serviceUser.dangkiUser(new UserModal(username, password, phone));
+            call.enqueue(new Callback<UserModal>() {
+                @Override
+                public void onResponse(Call<UserModal> call, Response<UserModal> response) {
+                    Toast.makeText(DangKyActivity.this, "Đăng ký thành công", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(DangKyActivity.this, DangNhapActivity.class);
+                    startActivity(intent);
+                }
+
+                @Override
+                public void onFailure(Call<UserModal> call, Throwable t) {
+                    Toast.makeText(DangKyActivity.this, "Đăng ký thất bại", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
     }
 }
--- app/src/main/java/account/fpoly/s_shop_client/DangNhapActivity.java
+++ app/src/main/java/account/fpoly/s_shop_client/DangNhapActivity.java
@@ -9,9 +9,28 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import account.fpoly.s_shop_client.Modal.UserModal;
+import account.fpoly.s_shop_client.Service.ServiceUser;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class DangNhapActivity extends AppCompatActivity {
     TextView tvDangKy;
     Button btnDangNhap;
+    private TextInputEditText txtuser, txtpass;
+    private List<UserModal> listUser = new ArrayList<>();
+    private UserModal mUser;
+    private Retrofit retrofit;
+    private ServiceUser serviceUser;
+    private String url = "http://192.168.1.7:3000";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +40,18 @@ public class DangNhapActivity extends AppCompatActivity {
         tvDangKy = findViewById(R.id.tv_dangky);
         btnDangNhap = findViewById(R.id.btn_DangNhap);
 
+        txtuser = findViewById(R.id.txt_username);
+        txtpass = findViewById(R.id.txt_password);
+
+        GetListUser();
+
         btnDangNhap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(DangNhapActivity.this, Tab_Giaodien_Activity.class);
                 startActivity(intent);
+
+                DangNhap();
 
                 Toast.makeText(DangNhapActivity.this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
             }
@@ -37,6 +63,61 @@ public class DangNhapActivity extends AppCompatActivity {
                 startActivity(intent);
 
                 overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+            }
+        });
+    }
+
+    private void DangNhap() {
+        String username = txtuser.getText().toString().trim();
+        String password = txtpass.getText().toString().trim();
+
+        boolean isHasUser = false;
+
+        if (listUser == null || listUser.isEmpty()) {
+            return;
+        }
+
+        for (UserModal user : listUser) {
+            if (username.equals(user.getUsername()) && password.equals(user.getPassword())) {
+                isHasUser = true;
+                mUser = user;
+                Toast.makeText(this, "Đăng nhập thành công", Toast.LENGTH_SHORT).show();
+                break;
+            } else if (username.equals("") && password.equals("")) {
+                Toast.makeText(this, "Không được để trống", Toast.LENGTH_SHORT).show();
+                break;
+            } else {
+
+            }
+        }
+
+        if (isHasUser) {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.putExtra("username", mUser.getUsername());
+            intent.putExtra("email", mUser.getEmail());
+            intent.putExtra("idUser", mUser.getId());
+            intent.putExtra("anhdaidien", mUser.getAnhdaidien());
+            intent.putExtra("password", mUser.getPassword());
+            intent.putExtra("hoten", mUser.getHoten());
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Tài khoản hoặc mật khẩu không đúng", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void GetListUser(){
+        retrofit = new Retrofit.Builder().baseUrl(url).addConverterFactory(GsonConverterFactory.create()).build();
+        serviceUser = retrofit.create(ServiceUser.class);
+        Call<List<UserModal>> call = serviceUser.getallUser();
+        call.enqueue(new Callback<List<UserModal>>() {
+            @Override
+            public void onResponse(Call<List<UserModal>> call, Response<List<UserModal>> response) {
+                listUser = response.body();
+            }
+
+            @Override
+            public void onFailure(Call<List<UserModal>> call, Throwable t) {
+                Toast.makeText(DangNhapActivity.this, "Lỗi", Toast.LENGTH_SHORT).show();
             }
         });
     }
