cf7330c35d995d9f1e1e5087a8d600a9446a4756
PaTuanDev203 <tuanpaph26696@gmail.com>
2023-10-03 10:36:16 +0700

    Tuanpaph26696 dev UI-UX History Screen

--- app/src/main/java/account/fpoly/s_shop_client/fragment/LichsuFragment.java
+++ app/src/main/java/account/fpoly/s_shop_client/fragment/LichsuFragment.java
@@ -18,7 +18,7 @@ public class LichsuFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =inflater.inflate(R.layout.activity_history_order_client, container, false);
+        View view =inflater.inflate(R.layout.fragment_lichsu, container, false);
 
 
         return view;
