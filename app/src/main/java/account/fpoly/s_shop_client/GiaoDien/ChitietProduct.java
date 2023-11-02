1d1635531c29390a6fd369f455b471819bb83c00
vanhai0k <hainvph26725@fpt.edu.vn>
2023-11-02 10:08:23 +0700

    hai_push_size,quantity

--- app/src/main/java/account/fpoly/s_shop_client/GiaoDien/ChitietProduct.java
+++ app/src/main/java/account/fpoly/s_shop_client/GiaoDien/ChitietProduct.java
@@ -4,17 +4,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -23,12 +26,19 @@ import com.bumptech.glide.Glide;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import account.fpoly.s_shop_client.API.API_Product;
 import account.fpoly.s_shop_client.Message;
 import account.fpoly.s_shop_client.Modal.ProductModal;
+import account.fpoly.s_shop_client.Modal.UserModal;
 import account.fpoly.s_shop_client.MuaProduct;
 import account.fpoly.s_shop_client.R;
 import account.fpoly.s_shop_client.Service.ServiceProduct;
+import account.fpoly.s_shop_client.adapter.ProductAdapter;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -39,24 +49,34 @@ public class ChitietProduct extends AppCompatActivity {
 
     LinearLayout clickmua,chat;
     ImageView back;
+    TextView chitiet_tenProduct,chitiet_giaProduct,chitiet_description;
     private String url = "http://192.168.1.10:3000";
     private  int sol = 1;
+    ProductModal productModal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chitiet_product);
 
-        Intent intent = getIntent();
-        String id = intent.getStringExtra("id");
-        String tenProduct = intent.getStringExtra("tenProduct");
-        String giaProduct = intent.getStringExtra("giaProduct");
-        String anhProduct = intent.getStringExtra("anhProduct");
+        SharedPreferences sharedPreferences = getSharedPreferences("product", MODE_PRIVATE);
 
-        getListThongTinProduct(id);
 
         clickmua = findViewById(R.id.clickmua);
         back = findViewById(R.id.back);
         chat = findViewById(R.id.chat);
+        chitiet_giaProduct = findViewById(R.id.chitiet_giaProduct);
+        chitiet_tenProduct = findViewById(R.id.chitiet_tenProduct);
+        chitiet_description = findViewById(R.id.chitiet_description);
+
+
+        String priceProduct = sharedPreferences.getString("giaProduct", null);
+        chitiet_giaProduct.setText(priceProduct);
+        String tenProduct = sharedPreferences.getString("tenProduct", null);
+        chitiet_tenProduct.setText(tenProduct);
+        String chitietProduct = sharedPreferences.getString("descriptionPro", null);
+        chitiet_description.setText(chitietProduct);
+
+
         chat.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -77,20 +97,22 @@ public class ChitietProduct extends AppCompatActivity {
                 View bottomView = LayoutInflater.from(getApplicationContext()).inflate(R.layout.layout_bottom_dialog,
                         (LinearLayout)findViewById(R.id.bottomSheetDialog));
 
-                getListThongTinProduct(id);
-
                 RadioButton checkBoxSize40 = bottomView.findViewById(R.id.checkBoxSize40);
                 RadioButton checkBoxSize41 = bottomView.findViewById(R.id.checkBoxSize41);
                 TextView buttonMinus = bottomView.findViewById(R.id.buttonMinus );
                 TextView buttonPlus = bottomView.findViewById(R.id.buttonPlus);
+                TextView totalQuantity = bottomView.findViewById(R.id.totalQuantity);
                 EditText edsoluong = bottomView.findViewById(R.id.numberPickerQuantity);
 
                 ImageView checkboxImgProduct = bottomView.findViewById(R.id.checkbox_ImageProduct);
                 TextView checkboxPriceProduct = bottomView.findViewById(R.id.checkbox_PriceProduct);
 
-                SharedPreferences sharedPreferences = getSharedPreferences("product", MODE_PRIVATE);
                 String priceProduct = sharedPreferences.getString("giaProduct", null);
                 checkboxPriceProduct.setText(priceProduct);
+                String totalQuantityProduct = sharedPreferences.getString("quantityPro", null);
+                totalQuantity.setText(totalQuantityProduct);
+
+
 
                 String imageProduct = sharedPreferences.getString("anhProduct", null);
                 Glide.with(getApplicationContext()).load(imageProduct).into(checkboxImgProduct);
@@ -129,6 +151,50 @@ public class ChitietProduct extends AppCompatActivity {
                     }
                 });
 
+//                checkBoxSize40.setText(sizePro);
+
+//--------------
+
+
+                LinearLayout layout = bottomView.findViewById(R.id.checkboxLayout);
+
+                ArrayList<String> selectedSizes = getIntent().getStringArrayListExtra("selectedSizes");
+                Toast.makeText(ChitietProduct.this, "[] Size: "+ selectedSizes, Toast.LENGTH_SHORT).show();
+                if (selectedSizes != null) {
+                    // Tạo một mảng CheckBox để lưu trữ các ô checkbox
+                    CheckBox[] checkBoxes = new CheckBox[selectedSizes.size()];
+
+                    // Hiển thị danh sách size lên các ô checkbox
+                    for (int i = 0; i < selectedSizes.size(); i++) {
+                        String size = selectedSizes.get(i);
+                        CheckBox checkBox = new CheckBox(getBaseContext());
+                        checkBox.setText(size);
+                        checkBoxes[i] = checkBox;
+                        // Thêm checkBox vào layout của bạn
+                        layout.addView(checkBox);
+                    }
+
+                    // Xử lý sự kiện khi người dùng chọn/deselect ô checkbox
+                    for (CheckBox checkBox : checkBoxes) {
+                        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                            @Override
+                            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                                // Xử lý logic khi người dùng chọn/deselect ô checkbox
+                                // Ví dụ: lưu trạng thái của ô checkbox vào một danh sách khác
+                            }
+                        });
+                    }
+                } else {
+                    // Xử lý khi danh sách size không tồn tại
+                    // Ví dụ: Hiển thị thông báo lỗi
+                    Toast.makeText(getBaseContext(), "Không có danh sách size", Toast.LENGTH_SHORT).show();
+                }
+
+
+
+
+//-------------
+
 
                 checkBoxSize40.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                     @Override
@@ -165,6 +231,8 @@ public class ChitietProduct extends AppCompatActivity {
                 bottomSheetDialog.setContentView(bottomView);
                 bottomSheetDialog.show();
             }
+
+
         });
     }
 
--- app/src/main/java/account/fpoly/s_shop_client/adapter/ProductAdapter.java
+++ app/src/main/java/account/fpoly/s_shop_client/adapter/ProductAdapter.java
@@ -3,10 +3,14 @@ package account.fpoly.s_shop_client.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -14,8 +18,13 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import account.fpoly.s_shop_client.GiaoDien.ChitietProduct;
 import account.fpoly.s_shop_client.API.API;
@@ -25,6 +34,7 @@ import account.fpoly.s_shop_client.Service.IClickItemListener;
 
 public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductViewHoder> {
     private final List<ProductModal> list;
+    List<ProductModal.Size> listSize;
     private final Context context;
     private final IClickItemListener iClickItemListener;
     public ProductAdapter(List<ProductModal> list, Context context, IClickItemListener iClickItemListener) {
@@ -33,6 +43,8 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
         this.iClickItemListener = iClickItemListener;
         notifyDataSetChanged();
     }
+
+
     @NonNull
     @Override
     public ProductViewHoder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -44,22 +56,94 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
     public void onBindViewHolder(@NonNull ProductViewHoder holder, int position) {
         ProductViewHoder productViewHoder = holder;
         ProductModal productModal = list.get(position);
+
         productViewHoder.NameProduct.setText("" + productModal.getName());
         productViewHoder.PriceProduct.setText("" + productModal.getPrice());
 
+
+        Picasso.get().load(API.api_reg + productModal.getImage()).into(holder.ImageProduct);
+
+        int totalQuantity = 0;
+        for (ProductModal.Size size : productModal.getSizes()) {
+            totalQuantity += size.getQuantity();
+        }
+
+//        StringBuilder sizesBuilder = new StringBuilder();
+//        String sizeLog = null;
+//        for (ProductModal.Size size : productModal.getSizes()) {
+//            sizesBuilder.append(size.getSize()).append(", ");
+//            sizeLog = String.valueOf(size.getSize());
+//        }
+//        String sizes = sizesBuilder.toString();
+//        if (sizes.length() > 0) {
+//            sizes = sizes.substring(0, sizes.length() - 2); // Loại bỏ dấu phẩy cuối cùng
+//        }
+//-------------
+        StringBuilder sizesBuilder = new StringBuilder();
+        for (ProductModal.Size size : productModal.getSizes()) {
+            sizesBuilder.append(size.getSize()).append(", ");
+            CheckBox checkBox = new CheckBox(context);
+            checkBox.setText(String.valueOf(size.getSize()));
+
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked) {
+                        // Kích thước đã được chọn
+                        String selectedSize = buttonView.getText().toString();
+                        Toast.makeText(context, "Đã chọn kích thước: " + selectedSize, Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Kích thước đã bị bỏ chọn
+                        String deselectedSize = buttonView.getText().toString();
+                        Toast.makeText(context, "Đã bỏ chọn kích thước: " + deselectedSize, Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            holder.checkboxLayout.addView(checkBox);
+        }
+        String sizes = sizesBuilder.toString();
+        if (sizes.length() > 0) {
+            sizes = sizes.substring(0, sizes.length() - 2); // Loại bỏ dấu phẩy cuối cùng
+        }
+//--    -------
+
+        holder.sizePro.setText(String.valueOf(sizes));
+
+        holder.totalQuantity.setText(String.valueOf(totalQuantity));
+
         String id = productModal.getId();
+        String sluong = String.valueOf(totalQuantity);
+        String description = productModal.getDescription();
+        String sizePro = String.valueOf(sizes);
 
-        Glide.with(context).load(productModal.getImage()).into(holder.ImageProduct);
-
+        Log.d("TAG", "sizeeeee: "+ sizePro);
+        String finalSizes = sizes;
         productViewHoder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+//                Log.d("TAG", "sizeeeee: "+ finalSizes);
                 SharedPreferences sharedPreferences = context.getSharedPreferences("product", context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPreferences.edit();
                 editor.putString("idProduct", id);
                 editor.putString("tenProduct", productModal.getName());
                 editor.putString("giaProduct", productModal.getPrice());
                 editor.putString("anhProduct", productModal.getImage());
+                editor.putString("quantityPro", sluong);
+                editor.putString("descriptionPro", description);
+                editor.putString("sizeProPro", sizePro);
+
+
+                for (ProductModal.Size size : productModal.getSizes()) {
+                    String sizeKey = "size_" + size.getSize();
+                    editor.putBoolean(sizeKey, true);
+                    Log.d("TAG", "sizeeeee: "+ sizeKey);
+//                    editor.putString ("size_", sizeKey);
+
+                }
+
+
                 editor.apply();
         ProductModal sp = list.get(position);
         if (sp == null){
@@ -71,6 +155,17 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(context, ChitietProduct.class);
+                ArrayList<String> selectedSizes = new ArrayList<>();
+
+                for (ProductModal.Size size : productModal.getSizes()) {
+                    String sizeKey = "size_" + size.getSize();
+                    editor.putBoolean(sizeKey, true);
+                    Log.d("TAG", "sizeeeee: "+ sizeKey);
+                    selectedSizes.add(sizeKey);
+                    Log.d("TAG", "sele[]"+ selectedSizes );
+                }
+//                intent.putStringArrayListExtra("selectedSizes", new ArrayList<>(productModal.getSizes()));
+
                 context.startActivity(intent);
 
             }
@@ -80,22 +175,6 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
             }
         });
 
-
-        int totalQuantity = 0;
-        for (ProductModal.Size size : productModal.getSizes()) {
-            totalQuantity += size.getQuantity();
-        }
-        StringBuilder sizesBuilder = new StringBuilder();
-        for (ProductModal.Size size : productModal.getSizes()) {
-            sizesBuilder.append(size.getSize()).append(", ");
-        }
-        String sizes = sizesBuilder.toString();
-        if (sizes.length() > 0) {
-            sizes = sizes.substring(0, sizes.length() - 2); // Loại bỏ dấu phẩy cuối cùng
-        }
-        holder.sizePro.setText(String.valueOf(sizes));
-
-        holder.totalQuantity.setText(String.valueOf(totalQuantity));
     }
 
     @Override
@@ -106,7 +185,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
     public class ProductViewHoder extends RecyclerView.ViewHolder {
         private TextView NameProduct, PriceProduct,totalQuantity,sizePro,Description;
         private ImageView ImageProduct;
-
+        LinearLayout checkboxLayout;
 
         public ProductViewHoder(@NonNull View itemView) {
             super(itemView);
@@ -116,6 +195,7 @@ public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.ProductV
             ImageProduct=itemView.findViewById(R.id.img_product);
             sizePro=itemView.findViewById(R.id.sizePro);
             Description=itemView.findViewById(R.id.chitiet_description);
+            checkboxLayout=itemView.findViewById(R.id.checkboxLayout);
         }
     }
 }
