dcedc80f8df372c6c7efed1f6c0a8768cf8deb92
vanhai0k <hainvph26725@fpt.edu.vn>
2023-11-20 17:54:32 +0700

    hai_push_UpdateImage

--- app/src/main/java/account/fpoly/s_shop_client/API/API_User.java
+++ app/src/main/java/account/fpoly/s_shop_client/API/API_User.java
@@ -4,13 +4,16 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import account.fpoly.s_shop_client.Modal.UserModal;
+import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.PUT;
+import retrofit2.http.Part;
 import retrofit2.http.Path;
 
 public interface API_User {
@@ -33,4 +36,12 @@ public interface API_User {
     Call<UserModal> postUser (@Body UserModal userModal);
     @POST("logout")
     Call<Void> logout();
+
+
+    @Multipart
+    @PUT("updateUser/{id}")
+    Call<UserModal> updateImage(@Path("id") String id,
+                                @Part("user") UserModal user,
+                                @Part MultipartBody.Part image);
+
 }
