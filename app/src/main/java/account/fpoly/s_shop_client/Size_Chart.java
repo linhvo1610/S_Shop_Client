37b05bfc5f433a4141ec95d735bb7303e5e76b39
vanhai0k <hainvph26725@fpt.edu.vn>
2023-12-08 14:01:04 +0700

    hai_UI

---
+++ app/src/main/java/account/fpoly/s_shop_client/Size_Chart.java
@@ -0,0 +1,24 @@
+package account.fpoly.s_shop_client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Size_Chart extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_size_chart);
+
+        ImageView back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
